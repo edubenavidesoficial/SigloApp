6afe549ec1ea6fd2557a0613cefaa734121c1378
edubenavidesoficial <edu3196.eb>
2025-04-02 23:49:40 -0500

    Consumo de Print

---
+++ SigloApp/SigloApp/Core/Network/PrintService.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 4/2/25.
+//
+
+import Foundation
---
+++ SigloApp/SigloApp/Infrastructure/Constants.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct API {
+    static let baseURL = "https://www.elsiglodetorreon.com.mx/api/app/v1"
+}
