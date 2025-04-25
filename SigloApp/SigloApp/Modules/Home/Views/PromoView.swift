f26597798a0c46d1923dee0a5603e3f851ed7afb
edubenavidesoficial <edu3196.eb>
2025-04-25 17:05:56 -0500

    Cambios en inicio

---
+++ SigloApp/SigloApp/Modules/Home/Views/PromoView.swift
@@ -0,0 +1,8 @@
+//
+//  PromoView.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 4/25/25.
+//
+
+import Foundation
---
+++ SigloApp/SigloApp/Modules/Profile/Views/MenuTopView.swift
@@ -0,0 +1,8 @@
+//
+//  MenuTopView.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 4/25/25.
+//
+
+import Foundation
