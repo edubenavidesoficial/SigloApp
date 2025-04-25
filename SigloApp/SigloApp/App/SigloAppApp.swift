f647a4c074b23bf25710bb753b2e3bca3037e461
edubenavidesoficial <edu3196.eb>
2025-04-24 22:26:52 -0500

    Cambios

--- SigloApp/SigloApp/App/SigloAppApp.swift
+++ SigloApp/SigloApp/App/SigloAppApp.swift
@@ -3,14 +3,27 @@ import SwiftUI
 @main
 struct SigloAppApp: App {
     @AppStorage("isLoggedIn") private var isLoggedIn: Bool = false
-    @StateObject private var articleViewModel = ArticleViewModel()  // Inicializamos el viewModel a nivel de la app
+    @StateObject private var articleViewModel = ArticleViewModel()
+    @State private var showWelcomeView = true
 
     var body: some Scene {
         WindowGroup {
-            if isLoggedIn {
-                TabsLayoutView(articleViewModel: articleViewModel)
+            if showWelcomeView {
+                // Mostrar la vista de bienvenida por unos segundos
+                WelcomeView()
+                    .onAppear {
+                        // Ocultar la vista de bienvenida después de 3 segundos
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                            self.showWelcomeView = false
+                        }
+                    }
             } else {
-                TabsHomeLayoutView(articleViewModel: articleViewModel)
+                // Mostrar la vista correspondiente según si el usuario está logueado o no
+                if isLoggedIn {
+                    TabsLayoutView(articleViewModel: articleViewModel)
+                } else {
+                    TabsHomeLayoutView(articleViewModel: articleViewModel)
+                }
             }
         }
     }
--- SigloApp/SigloApp/Modules/Home/Views/WelcomeView.swift
+++ SigloApp/SigloApp/Modules/Home/Views/WelcomeView.swift
@@ -1,29 +1,57 @@
 import SwiftUI
+import CryptoKit
 
-struct OpenView: View {
-    @State private var navigateToHome = false // Estado para controlar la navegación
+struct WelcomeView: View {
+    @State private var navigateToHome = false
+    @AppStorage("authToken") private var authToken: String = ""
     
+    // Definir correoHash y passwordHash
+    let correoHash = "fd342f795271570137b05f4dc763fb08" // Correo demo hasheado
+    let passwordHash = "5f4dcc3b5aa765d61d8327deb882cf99" // Contraseña demo hasheada (por ejemplo: "password")
+
     var body: some View {
         NavigationStack {
             ZStack {
-                // Fondo blanco
                 Color.white
                     .edgesIgnoringSafeArea(.all)
-                
-                // Imagen en el centro
-                Image("inicio") // Asegúrate de tener la imagen llamada "llama" en tus recursos
+
+                Image("inicio")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
             }
             .onAppear {
-                // Temporizador que cambia a HomeView después de 8 segundos
+                Task {
+                    do {
+                        print("Iniciando proceso de login...")
+                        // Realiza el login y usa el valor de retorno
+                        let user = try await LoginService.login(username: correoHash, password: passwordHash)
+                        
+                        // Ahora puedes usar `user` si es necesario
+                        print("Login exitoso para el usuario: \(user.nombre) \(user.apellidos)")
+                        
+                        // Guardar el token en el almacenamiento
+                        if let token = TokenService.shared.getStoredToken() {
+                            authToken = token
+                            print("🔐 Token obtenido y guardado: \(token)")
+                        } else {
+                            print("❌ No se obtuvo el token")
+                        }
+
+                    } catch {
+                        print("❌ Error obteniendo el token o realizando el login: \(error)")
+                    }
+
+                }
+
+                // Navegar a HomeView después de 8 segundos
                 DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
+                    print("Navegando a HomeView...")
                     self.navigateToHome = true
                 }
             }
             .navigationDestination(isPresented: $navigateToHome) {
-                HomeView() // Vista de destino cuando el estado cambia
+                HomeView()
             }
         }
     }
