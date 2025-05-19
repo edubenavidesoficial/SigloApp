23fcd6bb00edc40a9e7889c546c9d717650f5de6
edubenavidesoficial <edu3196.eb>
2025-05-19 18:28:20 -0500

    Cambios Visuales

---
+++ SigloApp/SigloApp/Modules/Profile/Views/SubView/CommentsView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct CommentsView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Ciclón John toca tierra por segunda vez, ahora en Michoacán; ha dejado al menos 16 muertos")
+                .font(.headline)
+                .lineLimit(nil)
+
+            HStack {
+                Text("Lo peor de todo es que se espera que otro huracán toque tierra en las costas de Guerrero.")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .lineLimit(nil)
+
+                Spacer()
+
+                Text("09:13 am, hoy")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding()
+    }
+}
