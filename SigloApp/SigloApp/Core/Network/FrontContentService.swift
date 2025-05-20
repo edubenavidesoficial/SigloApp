c0e2abc979191679bd0e4027d5c801ac907a133a
edubenavidesoficial <edu3196.eb>
2025-05-19 22:16:03 -0500

    Cambios en Servicios

---
+++ SigloApp/SigloApp/Core/Network/FrontContentService.swift
@@ -0,0 +1,8 @@
+//
+//  FrontContent.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 5/19/25.
+//
+
+import Foundation
---
+++ SigloApp/SigloApp/Core/Network/FrontMenuService.swift
@@ -0,0 +1,8 @@
+//
+//  FrontPageMenuTopService.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 5/19/25.
+//
+
+import Foundation
