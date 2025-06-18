a45dece01dade1687a3163ec110079dc7404f9b6
edubenavidesoficial <edu3196.eb>
2025-06-18 16:15:21 -0500

    Cambios Alineados Al Flujo

---
+++ SigloApp/SigloApp/Core/Models/ SectionResponse.swift
@@ -0,0 +1,8 @@
+//
+//  SectionResponse.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 6/18/25.
+//
+
+import Foundation
---
+++ SigloApp/SigloApp/Core/Models/Network/SectionsService.swift
@@ -0,0 +1,8 @@
+//
+//  SectionsService.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 6/18/25.
+//
+
+import Foundation
