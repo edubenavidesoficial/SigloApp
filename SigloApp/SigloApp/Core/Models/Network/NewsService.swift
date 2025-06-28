5ec5fc8391f5c7ae992827175f1a67cc4ee817d4
edubenavidesoficial <edu3196.eb>
2025-06-28 11:53:10 -0500

    Cambios Nuevos de Noticias

---
+++ SigloApp/SigloApp/Core/Models/Network/NewsService.swift
@@ -0,0 +1,8 @@
+//
+//  NewsService.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 6/28/25.
+//
+
+import Foundation
---
+++ SigloApp/SigloApp/Core/Response/NewsResponse.swift
@@ -0,0 +1,8 @@
+//
+//  NewsResponse.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 6/28/25.
+//
+
+import Foundation
