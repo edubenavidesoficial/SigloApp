735459c18f17dda200f7785d66687d3371e9b42e
edubenavidesoficial <edu3196.eb>
2025-07-03 14:33:01 -0500

    Nuevos Cambios Avisos

---
+++ SigloApp/SigloApp/Modules/Classifieds/ViewModels/AdDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AdDetailViewModel.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 7/3/25.
+//
+
+import Foundation
---
+++ SigloApp/SigloApp/Modules/Classifieds/ViewModels/AdsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AdsViewModel.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 7/3/25.
+//
+
+import Foundation
