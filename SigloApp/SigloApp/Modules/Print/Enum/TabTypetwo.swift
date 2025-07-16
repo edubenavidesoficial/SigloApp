a79e759783755de348e0ea908d3d505d0564f582
edubenavidesoficial <edu3196.eb>
2025-07-16 15:19:03 -0500

    Cambios de fuentes y Video Tv

---
+++ SigloApp/SigloApp/Modules/Print/Enum/TabTypetwo.swift
@@ -0,0 +1,7 @@
+// Tu enum de pestañas
+enum TabTypetwo: String, CaseIterable, Identifiable {
+    case hemeroteca = "HEMEROTECA EL SIGLO DE TORREÓN"
+    case suplementos = "SUPLEMENTOS"
+    case descargas = "MIS DESCARGAS"
+    var id: String { rawValue }
+}
---
+++ SigloApp/SigloApp/Modules/Sections/ViewModels/SectionMinutesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SectionMinutesViewModel.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 7/4/25.
+//
+
+import Foundation
