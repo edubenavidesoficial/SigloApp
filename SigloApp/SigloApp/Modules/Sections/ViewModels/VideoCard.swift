ea92aab5e44322b6b49a2611c4b4c0e13bdeb6b8
edubenavidesoficial <edu3196.eb>
2025-07-17 11:58:59 -0500

    Nuevos Cambios

---
+++ SigloApp/SigloApp/Modules/Sections/ViewModels/VideoCard.swift
@@ -0,0 +1,8 @@
+//
+//  VideoCard.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 7/16/25.
+//
+
+import Foundation
---
+++ SigloApp/SigloApp/Presentation/Components/BlackHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  BlackHeaderView.swift
+//  SigloApp
+//
+//  Created by Macbook Pro 17 i5R on 7/16/25.
+//
+
+import Foundation
